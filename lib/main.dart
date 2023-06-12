ab88381e33589d34041c7609f494697034e728d7
darkwingdck <dzakharov3819@gmail.com>
2023-06-12 16:33:10 +0300

    feat: bottom nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -68,34 +68,31 @@ class _MyHomePageState extends State<MyHomePage> {
     }
 
     return Scaffold(
-      body: Row(
+      body: Column(
         children: [
-          SafeArea(
-            child: NavigationRail(
-              extended: false,
-              destinations: [
-                NavigationRailDestination(
-                  icon: Icon(Icons.home),
-                  label: Text('Home'),
-                ),
-                NavigationRailDestination(
-                  icon: Icon(Icons.favorite),
-                  label: Text('Favorites'),
-                ),
-              ],
-              selectedIndex: selectedIndex,
-              onDestinationSelected: (value) {
-                setState(() {
-                  selectedIndex = value;
-                });
-              },
-            ),
-          ),
           Expanded(
             child: Container(
               color: Theme.of(context).colorScheme.primaryContainer,
               child: page,
             ),
+          ),
+          BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.favorite),
+                label: 'Favourite',
+              ),
+            ],
+            currentIndex: selectedIndex,
+            onTap: (value) => {
+              setState(() {
+                selectedIndex = value;
+              })
+            },
           ),
         ],
       ),
